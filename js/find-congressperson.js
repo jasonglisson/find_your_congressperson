c9e41ad5fbadcf2cd9d43b3fc96382d5b4e7dc08
Jason Glisson <jasonglisson@Jasons-MacBook-Air.local>
2014-10-13 22:39:27 -0400

    Changed "Congressman" to "Congressperson"

--- js/find-congressperson.js
+++ js/find-congressperson.js
@@ -2,7 +2,7 @@ jQuery(function() {
 
 	jQuery('.zip-button').click(function() {
 		var zipcode = jQuery('#zipcode-value').val();
-		var ziplink = "find_congressman?zip=" + zipcode ;	
+		var ziplink = "find_congressperson?zip=" + zipcode ;	
 		window.location.href = ziplink;
 		return false;
 	});
